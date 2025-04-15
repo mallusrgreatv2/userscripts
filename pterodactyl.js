2c34f80944e713484bbbe72049730c063e11c365
mallusrgreatv2 <mallusrgreat@gmail.com>
2025-04-15 13:31:50 +0530

    (pterodactyl) add license

--- pterodactyl.js
+++ pterodactyl.js
@@ -7,6 +7,7 @@
 // @match        https://*/server/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pterodactyl.io
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (async function () {
